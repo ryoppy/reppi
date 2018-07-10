416ff54bb45082562191b2894e86a82dab8cfe86
akito-kimura <akimura0128@gmail.com>
2018-07-11 01:56:58 +0900

    setup.py追加

---
+++ setup.py
@@ -0,0 +1,10 @@
+setup(
+    name='reppi',
+    version='0.0.1',
+    description='Application for keeping household accounts by taking picture of receipt.',
+    long_description=readme,
+    author='Akito Kimura',
+    install_requires=['requests', 'PyMySQL', ''],
+    url='https://github.com/akito-kimura/reppi',
+    license=license
+)
